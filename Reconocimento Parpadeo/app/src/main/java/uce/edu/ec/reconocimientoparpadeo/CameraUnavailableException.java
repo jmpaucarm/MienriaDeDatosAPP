98a4c592487c8fef5d01d0962952e0d2d74f8ad5
jmpaucarm <jmpaucarm@hotmail.com>
2018-12-27 20:22:10 -0500

    Parpadeo

---
+++ Reconocimento Parpadeo/app/src/main/java/uce/edu/ec/reconocimientoparpadeo/CameraUnavailableException.java
@@ -0,0 +1,10 @@
+package uce.edu.ec.reconocimientoparpadeo;
+
+/**
+ * Created by muralikrishna on 02/11/17.
+ */
+
+public class CameraUnavailableException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
+
